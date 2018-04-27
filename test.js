2f3bccde580f04700076576a3d9ef14fbb1844f2
leiwei <523500710@qq.com>
2018-04-27 10:54:13 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,16 +1,16 @@
 let ProxyVerifier = require('proxy-verifier');
 
 let proxy = {
-	ipAddress: '112.115.57.20',
+ 	ipAddress: '112.115.57.20',
 	port: 3128,
 	protocol: 'http'
 };
 
 ProxyVerifier.testAll(proxy, function(error, result) {
 	if (error) {
-        console.log('proxyVerifiier error ', error);
+        	console.log('proxyVerifiier error ', error);
 	} else {
-        console.log(JSON.stringify(result));
+        	console.log(JSON.stringify(result));
 		// {"anonymityLevel":"anonymous","tunnel":{"ok":true},"protocols":{"http":{"ok":true}}}
 	}
 });
